ef9c140354f1b2446aeec1f96adb0e0d2c33f4a9
Cora Yerke <bange1cm@cmich.edu>
2026-02-21 14:01:46 -0500

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,53 @@
+//create a variable
+let x = 5;
+
+//create a constant
+const Y = 10;
+
+//--------------------------------------------------------------------
+
+//arrays
+// let array = ["blue", "pink", "green"];
+// console.log(array[0]); // Output: blue
+
+// array_nums = [1, 4, 7, 2, 5];
+// console.log(array_nums);
+// array_nums.map((x) => x * 2);
+// console.log(array_nums);
+
+//--------------------------------------------------------------------
+
+//functions
+// function add(a, b) {
+//  //write the code!
+// }
+// console.log(add(3, 4)); // Output: 7
+
+//now try to write it as an arrow function
+
+//--------------------------------------------------------------------
+//if statements
+// let num = 10;
+// if (num > 5) {
+//   console.log("num is greater than 5");
+// } else {
+//   console.log("num is less than or equal to 5");
+// }
+
+//--------------------------------------------------------------------
+//loops
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+//--------------------------------------------------------------------
+//objects
+// const person = {
+//   name: "John",
+//   age: 30,
+//   greet: function () {
+//     console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+//   },
+// };
+
+// person.greet();
